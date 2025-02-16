97fe73dd6dfc65e421d415604b7f69015f5ef06f
Dmitrii A <alekseevsolutions@gmail.com>
2025-02-15 18:07:32 -0800

    authentication integrated

--- restate-app/app/sign-in.tsx
+++ restate-app/app/sign-in.tsx
@@ -1,19 +1,37 @@
-import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native'
+import {Image, ScrollView, Text, TouchableOpacity, View, Alert} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import {login} from "@/lib/appwrite"
+import {tls} from "node-forge";
+import {useGlobalContext} from "@/lib/global-provider";
+import {Redirect} from "expo-router";
 
-const SignIn = () => {
-    const handleLogin= () => {}
-    // @ts-ignore
+const SignIn = async () => {
+    const { refetch, loading, isLoggedIn } = useGlobalContext();
+
+    if (!loading && isLoggedIn ) return <Redirect href="/" />
+
+    const handleLogin = async () => {
+
+        const result = await login();
+
+        if (result) {
+            refetch();
+        } else {
+            Alert.alert('Error', 'Failed to login');
+        }
+    }
+
     return (
         <SafeAreaView className="bg-white h-full">
             <ScrollView contentContainerClassName="h-full">
-                <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain" />
+                <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain"/>
 
                 <View className="px-10">
-                    <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to ReState</Text>
+                    <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to
+                        ReState</Text>
                     <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
                         Let's Get You Closer to {"\n"}
                         <Text className="text-primary-300">Your Ideal Home</Text>
@@ -23,10 +41,11 @@ const SignIn = () => {
                         Login to ReState with Google
                     </Text>
 
-                    <TouchableOpacity onPress={handleLogin} className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
+                    <TouchableOpacity onPress={handleLogin}
+                                      className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
 
                         <View className="flex flex-row items-center justify-center">
-                            <Image source={icons.google} className="w-5 h-5" resizeMode="contain" />
+                            <Image source={icons.google} className="w-5 h-5" resizeMode="contain"/>
                             <Text className="text-lg front-rubik-medium text-black-300 ml-2">Continue with Google</Text>
                         </View>
 
