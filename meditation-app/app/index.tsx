19c4f17ed756c3a7ab38626fd2d550569ebdf4aa
adm <alekseevsolutions@gmail.com>
2025-03-25 21:04:00 -0700

    nativewind installed

--- meditation-app/app/index.tsx
+++ meditation-app/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center" >
+      <Text className="text-4xl text-blue-700 border-2 p-5 border-amber-600 bg-amber-200 rounded-full">Privet</Text>
     </View>
   );
 }
